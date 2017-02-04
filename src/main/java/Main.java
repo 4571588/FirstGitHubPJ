d099c2204a0dc0292e93335a7d0f42c9d9f0a3be
zwq <457158874@qq.com>
2017-02-04 19:00:11 +0800

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2017/2/4 0004.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("This is my first project on github ! now i just try to test how to post the project to github!");
+    }
+
+}
